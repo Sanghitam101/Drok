3b9f324ec62eb70831e2775305f9044109e46c7b
Sanghitam101 <164131140+Sanghitam101@users.noreply.github.com>
2024-03-20 22:01:38 +0800

    Create Drak.sh

---
+++ Drak.sh
@@ -0,0 +1,30 @@
+
+#!/bin/bash
+echo
+tput bold
+tput setaf 40
+tput blink
+echo "$* **Fikrado.py is installing"
+tput sgr0
+tput sgr0
+tput sgr0
+echo
+echo "downloading python2 for hackers"
+apt install python2
+echo "downloading pip requrement"
+pip2 install mechanize requests
+python2 fikrado.py#!/bin/bash
+echo
+tput bold
+tput setaf 40
+tput blink
+echo "$* **Fikrado.py is installing"
+tput sgr0
+tput sgr0
+tput sgr0
+echo
+echo "downloading python2 for hackers"
+apt install python2
+echo "downloading pip requrement"
+pip2 install mechanize requests
+python2 fikrado.py
